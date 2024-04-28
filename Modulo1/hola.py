e25d65dc71dd60958726747b874348470ae83a77
KatherineLlv <168352561+KatherineLlv@users.noreply.github.com>
2024-04-28 15:18:04 +0000

    hola.py modificado

---
+++ Modulo1/hola.py
@@ -0,0 +1,7 @@
+#Ejecutando hola mundo
+
+print("hola mundo")
+#Comandos útiles
+#Clear ->Limpiar la terminal . Para windows clear<>cls
+#cd -> cambiar directorio sobre la terminal
+#python <nombre archivo>.py -> ejecutar mi programa en la terminal
