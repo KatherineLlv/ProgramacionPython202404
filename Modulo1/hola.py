b31af832864e5c120d70e1afb2091130177bf337
KatherineLlv <168352561+KatherineLlv@users.noreply.github.com>
2024-05-25 04:10:21 +0000

    clase1modifica.py

--- Modulo1/hola.py
+++ Modulo1/hola.py
@@ -5,3 +5,53 @@ print("hola mundo")
 #Clear ->Limpiar la terminal . Para windows clear<>cls
 #cd -> cambiar directorio sobre la terminal
 #python <nombre archivo>.py -> ejecutar mi programa en la terminal
+
+#Tipo de datos
+#int ->  número entero
+#float -> número decimal
+#str -> cadena de texto
+#bool -> valor lógico (Verdadero o Falso)
+
+#Operadores
+#suma
+#resta
+#multiplicación
+#exponente
+#división 17/5 = 3.4
+#division entera 17//5 = 3.0
+#residuo -> 17%5 = 3
+#redondear un numero a n decimales:
+round(3.759,1)
+
+#OPERADORES LÓGICOS (comparaciones)
+# igual a -> ==
+# diferente a -> !=
+# menor
+# menor e igual
+# mayor
+# mayor a igual
+
+#OPERADORES LÓGICAS "COMPLEJAS"
+# Función and
+# Promoción del club:
+# si es miembro del club y tiene más de 40 años se le regala un
+# masaje gratis
+A = int(input("Ingrese la edad del cliente: "))
+B= int(input(" Escribir (1) si es miembro del club o (0) si no es miembro"))
+
+A > 40 and B == 1:
+
+#Función or
+#Función not
+
+#Flujo de control
+#Condición if 
+# sentendia else <- si nada de lo anterior se cumpe != TRUE 
+# sentencia elif <- cuando hay varias condiciones
+
+#Ayuda a correccion de datos por el usuario
+#Si el usuario ingresa datos en minuscula cuando lo solicitado es en mayuscula
+#Usar comando upper
+uper (mamá)
+#si no 
+lower (mAmá)
